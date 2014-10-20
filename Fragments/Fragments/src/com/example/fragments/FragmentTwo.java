9fd39c80cce780774f3e0d5f1632384443928e11
albertowusuasare <owusuasa@grinnell.edu>
2014-10-19 23:14:48 -0500

    Update Fragments -> Tutorial is updated :How do I use a fragment in my app? -> Android files for updated

---
+++ Fragments/Fragments/src/com/example/fragments/FragmentTwo.java
@@ -0,0 +1,19 @@
+package com.example.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentTwo extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		View viewHierarchy = inflater.inflate(R.layout.fragment_02, container, false);
+		
+		return viewHierarchy;
+	}
+
+}
