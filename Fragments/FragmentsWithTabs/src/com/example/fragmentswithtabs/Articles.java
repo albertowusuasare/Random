588d6c9a063df3d9db7d4d46620167cc12bb9ec5
albertowusuasare <owusuasa@grinnell.edu>
2014-10-21 09:22:51 -0500

    Implement tabs with swipe feature

---
+++ Fragments/FragmentsWithTabs/src/com/example/fragmentswithtabs/Articles.java
@@ -0,0 +1,20 @@
+package com.example.fragmentswithtabs;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Articles extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.fragment_articles, container, false);
+		
+		return rootView;
+	} // onCreateView
+}//Articles
--- Fragments/FragmentsWithTabs/src/com/example/fragmentswithtabs/MainActivity.java
+++ Fragments/FragmentsWithTabs/src/com/example/fragmentswithtabs/MainActivity.java
@@ -1,16 +1,65 @@
 package com.example.fragmentswithtabs;
 
-import android.support.v7.app.ActionBarActivity;
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
 
+	//AppSectionsPagerAdapter mAppSectionsPagerAdapter;
+	private ViewPager viewPager;
+	private TabsAdapter mAdapter;
+	private ActionBar actionBar;
+	// Tab titles
+	private String[] tabs = { "Articles", "Social Media", "RSS" };
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		// Initilization
+				viewPager = (ViewPager) findViewById(R.id.pager);
+				actionBar = getActionBar();
+				mAdapter = new TabsAdapter(getSupportFragmentManager());
+
+				viewPager.setAdapter(mAdapter);
+				actionBar.setHomeButtonEnabled(false);
+				actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);		
+
+				// Adding Tabs
+				for (String tab_name : tabs) {
+					actionBar.addTab(actionBar.newTab().setText(tab_name)
+							.setTabListener(this));
+				}
+
+				/**
+				 * on swiping the viewpager make respective tab selected
+				 * */
+				viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+					@Override
+					public void onPageSelected(int position) {
+						// on changing the page
+						// make respected tab selected
+						actionBar.setSelectedNavigationItem(position);
+					}
+
+					@Override
+					public void onPageScrolled(int arg0, float arg1, int arg2) {
+					}
+
+					@Override
+					public void onPageScrollStateChanged(int arg0) {
+					}
+				});
 	}
 
 	@Override
@@ -31,4 +80,25 @@ public class MainActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+   
+
 }
